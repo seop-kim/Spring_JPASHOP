fad672c4a3ce7fec8582754f2e8dea6ffcc21dd5
tseop <77163842+tseop@users.noreply.github.com>
2023-01-20 09:55:18 +0900

    feat: 엔티티 생성

---
+++ src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@DiscriminatorValue("M")
+public class Movie extends Item {
+    private String director;
+    private String actor;
+}
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jpabook.jpashop.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void testMember() {
+        // given
+        Member member = new Member();
+        member.setName("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        assertThat(findMember)
+                .isEqualTo(member);
+    }
+
+}
